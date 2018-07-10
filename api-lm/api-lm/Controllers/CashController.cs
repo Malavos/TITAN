c16d00a61c1cc3f06af71d96b2333ab3bb716d5f
Pedro Santos <pedro.santos@oleconsignado.onmicrosoft.com>
2018-07-10 17:12:20 -0300

    Partial  commit - Added the Cash Controller.

---
+++ api-lm/api-lm/Controllers/CashController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace api_lm
+{
+
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class CashController : Controller
+    {
+        [HttpGet]
+        [Route("login")]
+        [ProducesResponseType(201, Type = typeof(BsonDocument))]
+        [ProducesResponseType(HttpStatusCode.BadRequest)]
+        [ProducesResponseType(HttpStatusCode.NoContent)]
+
+
+
+    }
+}
